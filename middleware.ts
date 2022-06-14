fa3f91e17bae82a2267adcfb3100fa786008308b
Ramón Huidobro <hola@ramonh.dev>
2022-06-14 17:29:16 +0200

    Test and despair

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname === "/test") {
+  if (request.nextUrl.pathname === "/about") {
     return NextResponse.redirect(new URL("/", request.url));
   }
 }
